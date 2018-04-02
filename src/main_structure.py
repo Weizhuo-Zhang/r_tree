467a7fa4206ef7efa2b07f7504771ac7ea7079e2
Weizhuo Zhang <s4494858@labs.eait.uq.edu.au>
2018-04-02 14:49:08 +1000

    'finish_two_classes'

--- src/main_structure.py
+++ src/main_structure.py
@@ -1,3 +1,27 @@
+from Queue import PriorityQueue
+
+class point:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+class Node:
+    m = 3
+    min_m = 0.4 * 3
+    def __init__(self):
+        self.is_leaf = False
+        self.predecessor = None
+        self.successor = []
+        self.left = 0.0
+        self.right = 0.0
+        self.top = 0.0
+        self.down = 0.0
+
 data_file = open('data/dataset.txt')
+num_point = data_file.read()
 data = data_file.readlines()
 data_file.close()
+
+x_q = PriorityQueue()
+y_q = PriorityQueue()
+# for line in
